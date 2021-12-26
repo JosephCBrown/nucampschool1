c7ffe23c44df1a629325938bd7c1f9f400dfd493
Joseph Brown <jcbrown2012@gmail.com>
2021-12-26 14:18:28 -0800

    training data & pytorch implemented

--- 1-Fundamentals/Chatbotapp/nltk_utils.py
+++ 1-Fundamentals/Chatbotapp/nltk_utils.py
@@ -1,7 +1,9 @@
 import nltk
-#nltk.download('punkt') - you need to download this for popular data sets.
+import numpy as np
+# nltk.download('punkt') - you need to download this for popular data sets.
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
+
 
 def tokenize(sentance):
     return nltk.word_tokenize(sentance)
@@ -10,16 +12,33 @@ def tokenize(sentance):
 def stem(word):
     return stemmer.stem(word.lower())
 
-def list_of_words(sentence_tokenized, all_words):
-    pass
 
-"""#This is the test of the tokenization
+def list_of_words(sentence_tokenized, all_words):
+    sentence_tokenized = [stem(w) for w in sentence_tokenized]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in sentence_tokenized:
+            bag[idx] = 1.0
+    return bag
+
+
+"""---Test 1---- 
+#This is the test of the tokenization
 a = "how are you ?"
 print(a)
 a = tokenize(a)
 print(a)"""
 
-""" # Testting the word stemmer
+""" # ----Test 2----
+Testing the word stemmer
 words = ["organize", "Organizes", "organizing"]
 stem_words = [stem(w) for w in words]
 print(stem_words) """
+
+""" #----Test 3-----
+This is to test that training array works
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bog = list_of_words(sentence, words)
+print(bog) """
--- 1-Fundamentals/Chatbotapp/train.py
+++ 1-Fundamentals/Chatbotapp/train.py
@@ -1,5 +1,11 @@
 import json
 from nltk_utils import tokenize, stem, list_of_words
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -15,4 +21,41 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 ignore_words = ['?', '!', '.', ',']
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = list_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)  # CrossEntropyLoss Dont need 1 hot endcodding here
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+
+class ChatDatset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # allows you to access dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDatset()
+train_loader = DataLoader(
+    dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
