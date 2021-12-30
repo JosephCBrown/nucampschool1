a2058151f40b92ad1e5777449a67879739c52223
Joseph Brown <jcbrown2012@gmail.com>
2021-12-30 02:54:12 -0800

    The test file and incomplete wkshp 5

--- 1-Fundamentals/week5/test.py
+++ 1-Fundamentals/week5/test.py
@@ -1,3 +1,52 @@
-a_list = [2,3,4,5,6,7,8,9,12]
+import random
+def binarySearch(alist, item):
+        first = 0
+        last = len(alist) - 1
+        found = False
 
-print(a_list.index(7))
+        while first<=last and not found:
+            midpoint = (first + last)//2            
+            if alist[midpoint] == item:
+                found = True
+            else:
+                if item < alist[midpoint]:
+                    last = midpoint-1
+                else:
+                    first = midpoint+1  
+        return found
+
+def findThisNum(mynum):
+
+    testlist = [x for x in range(listlength)]
+
+    print("testlist = ", testlist)
+    print("finding number ", mynum)
+
+    if (binarySearch(testlist, findnum)) == True:
+        print("found %d" %mynum)
+    else:
+        print("Not found %d" %mynum)
+
+
+
+
+#### Main_Function ####
+
+if __name__ == "__main__":
+    #
+
+    #Search 1 [ Even numbered list ]
+    listlength = 10    
+    findnum = random.randrange(0,listlength)
+    findThisNum(findnum)     
+
+    #Search 2 [ [ Odd numbered list ]
+    listlength = 13    
+    findnum = random.randrange(0,listlength)
+    findThisNum(findnum)
+
+    #search 3  [ find item not in the list ]
+
+    listlength = 13    
+    findnum = random.randrange(0,listlength) + listlength
+    findThisNum(findnum)
--- 1-Fundamentals/week5/workshop5.py
+++ 1-Fundamentals/week5/workshop5.py
@@ -18,4 +18,67 @@ def guess_random_number(tries, start, stop):
             print(f"You have failed to guess the number: {r1}")
             return
 
-guess_random_number(5, 0, 11)
+# ---driver code test task 1----
+# guess_random_number(5, 0, 11)
+
+
+def guess_random_number_linear(tries, start, stop):
+    r1 = random.randint(start, stop)
+    c_guess = random.randrange(start, stop)
+    print(f"The number for the program to guess is: {r1}")
+
+    while tries > 0:
+        for i in [c_guess]:
+            print(f"Number of tries remaining: {tries}")
+            print(f"The program is guessing... {i}")
+            if i == r1:
+                return print(f"The program guessed the correct number!")
+        tries -= 1
+        if tries == 0:
+            print(
+                f"The program has failed to guess the correct number of: {r1}")
+            return -1
+    return
+
+# ---driver code test task 2----
+#guess_random_number_linear(5, 0, 11)
+
+
+def binarySearch(alist, item):
+    first = 0
+    last = len(alist) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+    return found
+
+
+def guess_random_num_binary(tries, start, stop):
+    r1 = random.randint(start, stop)
+    c_guess = [x for x in range(listlength)]
+    print(f"Random number to find: {r1}")
+    while tries > 0:
+        if (binarySearch(c_guess, findnum)) == r1:
+            print("Found it! %d" % r1)
+        if (binarySearch(c_guess, findnum)) > r1:
+            print(f"Guessing lower!")
+        if (binarySearch(c_guess, findnum)) < r1:
+            print(f"Guessing higher!")
+        tries -= 1
+        if tries == 0:
+            print(
+                f"The program has failed to guess the correct number of: {r1}")
+    return c_guess
+
+
+listlength = 101
+findnum = random.randrange(0, listlength)
+guess_random_num_binary(5, 0, 101)
